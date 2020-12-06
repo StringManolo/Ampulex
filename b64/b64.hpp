ea26ba1a52c6eb071e57ff8f4ad40e95e4ebe916
StringManolo <manuelvarelacaldas@gmail.com>
2020-12-06 13:06:19 +0100

    Fixed segfault in compressa path pointer

--- b64/b64.hpp
+++ b64/b64.hpp
@@ -1,37 +1,111 @@
-static std::string base64_encode(const std::string &in) {
+#pragma once
 
-    std::string out;
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <cstdint>
 
-    int val=0, valb=-6;
-    for (unsigned char c : in) {
-        val = (val<<8) + c;
-        valb += 8;
-        while (valb>=0) {
-            out.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[(val>>valb)&0x3F]);
-            valb-=6;
-        }
-    }
-    if (valb>-6) out.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[((val<<8)>>(valb+8))&0x3F]);
-    while (out.size()%4) out.push_back('=');
-    return out;
-}
+namespace base64
+{
+	inline static const char kEncodeLookup[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	inline static const char kPadCharacter = '=';
 
-static std::string base64_decode(const std::string &in) {
+	using byte = std::uint8_t;
 
-    std::string out;
+	inline std::string encode(const std::vector<byte>& input)
+	{
+		std::string encoded;
+		encoded.reserve(((input.size() / 3) + (input.size() % 3 > 0)) * 4);
 
-    std::vector<int> T(256,-1);
-    for (int i=0; i<64; i++) T["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[i]] = i; 
+		std::uint32_t temp{};
+		auto it = input.begin();
 
-    int val=0, valb=-8;
-    for (unsigned char c : in) {
-        if (T[c] == -1) break;
-        val = (val<<6) + T[c];
-        valb += 6;
-        if (valb>=0) {
-            out.push_back(char((val>>valb)&0xFF));
-            valb-=8;
-        }
-    }
-    return out;
+		for(std::size_t i = 0; i < input.size() / 3; ++i)
+		{
+			temp  = (*it++) << 16;
+			temp += (*it++) << 8;
+			temp += (*it++);
+			encoded.append(1, kEncodeLookup[(temp & 0x00FC0000) >> 18]);
+			encoded.append(1, kEncodeLookup[(temp & 0x0003F000) >> 12]);
+			encoded.append(1, kEncodeLookup[(temp & 0x00000FC0) >> 6 ]);
+			encoded.append(1, kEncodeLookup[(temp & 0x0000003F)      ]);
+		}
+
+		switch(input.size() % 3)
+		{
+		case 1:
+			temp = (*it++) << 16;
+			encoded.append(1, kEncodeLookup[(temp & 0x00FC0000) >> 18]);
+			encoded.append(1, kEncodeLookup[(temp & 0x0003F000) >> 12]);
+			encoded.append(2, kPadCharacter);
+			break;
+		case 2:
+			temp  = (*it++) << 16;
+			temp += (*it++) << 8;
+			encoded.append(1, kEncodeLookup[(temp & 0x00FC0000) >> 18]);
+			encoded.append(1, kEncodeLookup[(temp & 0x0003F000) >> 12]);
+			encoded.append(1, kEncodeLookup[(temp & 0x00000FC0) >> 6 ]);
+			encoded.append(1, kPadCharacter);
+			break;
+		}
+
+		return encoded;
+	}
+
+	std::vector<byte> decode(const std::string& input)
+	{
+		if(input.length() % 4)
+			throw std::runtime_error("Invalid base64 length!");
+
+		std::size_t padding{};
+
+		if(input.length())
+		{
+			if(input[input.length() - 1] == kPadCharacter) padding++;
+			if(input[input.length() - 2] == kPadCharacter) padding++;
+		}
+
+		std::vector<byte> decoded;
+		decoded.reserve(((input.length() / 4) * 3) - padding);
+
+		std::uint32_t temp{};
+		auto it = input.begin();
+
+		while(it < input.end())
+		{
+			for(std::size_t i = 0; i < 4; ++i)
+			{
+				temp <<= 6;
+				if     (*it >= 0x41 && *it <= 0x5A) temp |= *it - 0x41;
+				else if(*it >= 0x61 && *it <= 0x7A) temp |= *it - 0x47;
+				else if(*it >= 0x30 && *it <= 0x39) temp |= *it + 0x04;
+				else if(*it == 0x2B)                temp |= 0x3E;
+				else if(*it == 0x2F)                temp |= 0x3F;
+				else if(*it == kPadCharacter)
+				{
+					switch(input.end() - it)
+					{
+					case 1:
+						decoded.push_back((temp >> 16) & 0x000000FF);
+						decoded.push_back((temp >> 8 ) & 0x000000FF);
+						return decoded;
+					case 2:
+						decoded.push_back((temp >> 10) & 0x000000FF);
+						return decoded;
+					default:
+						throw std::runtime_error("Invalid padding in base64!");
+					}
+				}
+				else throw std::runtime_error("Invalid character in base64!");
+
+				++it;
+			}
+
+			decoded.push_back((temp >> 16) & 0x000000FF);
+			decoded.push_back((temp >> 8 ) & 0x000000FF);
+			decoded.push_back((temp      ) & 0x000000FF);
+		}
+
+		return decoded;
+	}
 }
